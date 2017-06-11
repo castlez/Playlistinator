c4b6a9f057798b5ca0bf807438147752ee577cb1
castlez <castlez93@gmail.com>
2017-06-10 20:29:52 -0700

    working

--- playlistinator.py
+++ playlistinator.py
@@ -2,7 +2,7 @@
 Playlistinator
 by Jonathan Castle
 
-Accepts a sentence from the user and produces 
+Accepts a sentence from the user and produces
 a playlist of songs that spell out the sentence
 """
 
@@ -15,8 +15,8 @@ import json
 def main():
 	ans = input("name a song > ")
 	while(ans != "q"):
-		isthere, artist = check_song(ans)
-		print(str(isthere) + " : artist = " + str(artist))
+		plylst = construct_playlist(ans)
+		print("playlist: \n" + str(plylst))
 		ans = input("name a song > ")
 
 def build_result(playlist):
@@ -26,26 +26,25 @@ def build_result(playlist):
 	return result
 
 # accepts a sentence from the user
-# and attempts to construct a 
-# playlist using the words in the 
+# and attempts to construct a
+# playlist using the words in the
 # sentence
 #
 # returns a list of song title/artist pairs
 def construct_playlist(sentence):
-	#TODO: need algorithm
-	
+
 	# split the sentence into tokens (words)
 	words = sentence.split(" ")
 
 	# LEXOGRAPHIC SEARCH
 		# starting with the whole sentence, try to
-		# find a song title. if one isn't found, 
+		# find a song title. if one isn't found,
 		# try the whole sentence minus the last word.
 		# etc.
 	isthere, artist = check_song(sentence.title())
 	if(isthere):
 		return [[str(sentence), str(artist)]]
-	
+
 	done = False
 	playlist = []
 
@@ -53,19 +52,19 @@ def construct_playlist(sentence):
 	# start with the sentence minus its last word
 	# and continue reducing size till you find a valid title
 	# then move to the next word and do it again
-	offset = len(words) - 1 
+	offset = len(words) - 1
 	while(done == False):
 		songtitle = " ".join(words[:offset]).title()
 		isthere, artist = check_song(songtitle)
 		if(isthere):
 			whatsleft = " ".join(words[offset:])
-			playlist = [[songtitle, artist]] + construct_playlist(whatsleft)
+			return [[songtitle, artist]] + construct_playlist(whatsleft)
 			done = True
 		else:
 			offset -= 1
 
 # checks if the given song title is
-# a valid song 
+# a valid song
 #
 # returns bool does_song_exists, string artist
 def check_song(song):
@@ -73,7 +72,7 @@ def check_song(song):
 	songtitle = song.title()
 	baseurl = "http://ws.audioscrobbler.com"
 	apikey = "4b93344a08fb38e05c0ebf6fe7c90361"
-	
+
 	# build query and submit, gather response
 	query = baseurl + "/2.0/?method=track.search&track=" + songtitle.replace(" ", "%20") + "&api_key=" + apikey + "&format=json"
 	print(str(query))
@@ -84,7 +83,7 @@ def check_song(song):
 	# otherwise return false and n/a
 	if(content['results']['trackmatches']['track'] == []):
 		return False, "N/A"
-	else: 
+	else:
 		for track in content['results']['trackmatches']['track']:
 			if(track['name'] == songtitle):
 				return True, track['artist']
